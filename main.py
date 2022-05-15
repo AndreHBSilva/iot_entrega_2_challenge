ad0b770c175a3a1950102bdf5eba704bd832ef5a
AndreHBSilva <andrehugo.bs@gmail.com>
2022-05-15 12:18:30 -0300

    aceitar comandos após chamar o nome da assistente

--- main.py
+++ main.py
@@ -24,23 +24,33 @@ def detectarPeriodoDoDia():
     return 'Boa noite'
 
 def cadastrarEventoAgenda():
+  falar('Cadastrando evento na agenda...')
   return 'Cadastrando evento na agenda...'
 
 def lerAgenda():
+  falar('Lendo agenda...')
   return 'Lendo agenda...'
 
 def checarHoras():
+  falar('Checando horas...')
   return 'Checando horas...'
 
 def mapearComandos(comando):
   comandos = {
-    ('ler agenda', 'quero ler minha agenda', 'abrir agenda'): lerAgenda,
+    ('ler agenda', 'ver agenda', 'quero ver agenda', 'quero ler minha agenda', 'abrir agenda'): lerAgenda,
     ('cadastrar evento na agenda', 'cadastrar novo evento na agenda', 'cadastrar evento'): cadastrarEventoAgenda,
     ('checar as horas', 'que horas são', 'checar horário'): checarHoras
   }
   for key, value in comandos.items():
     if comando.lower() in key:
       return value()
+    
+def ouvir(recon):
+  print('Fale algo...')
+  audio = recon.listen(source)
+  print('Reconhecendo...')
+  comando = recon.recognize_google(audio, language='pt')
+  return comando
 
 
 recon = sr.Recognizer()
@@ -50,20 +60,23 @@ with sr.Microphone() as source:
   recon.adjust_for_ambient_noise(source, duration=4)
   while True:
     try:
-      print(mapearComandos('QUERO LER MINHA AGENDA'))
-      falar(detectarPeriodoDoDia()+', mestre. Como posso ajudá-lo?')
-      print('Fale algo...')
-      audio = recon.listen(source)
-      print('Reconhecendo...')
-      comando = recon.recognize_google(audio, language='pt')
-      falar(comando)
-      print(comando)
-      esperar = input('Pressione Enter para continuar...')
+      comando = ouvir(recon)
+      if 'sexta-feira' in comando:
+        falar(detectarPeriodoDoDia()+', mestre. Como posso ajudá-lo?')
+        print(comando)
+        esperar = input('Pressione Enter para continuar...')
+        comando = ouvir(recon)
+        mapearComandos(comando)
+        print(comando)
+        esperar = input('Pressione Enter para continuar...')
     except sr.UnknownValueError:
       print('\nTente novamente')
       continue
     except KeyboardInterrupt:
       print('\nEncerrando...')
       break
+    except KeyError:
+      print('\n Comando não encontrado')
+      continue
 
 #print('hello world')
