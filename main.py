e76d7c9f78f9920392d27243008a455f992a82c9
AndreHBSilva <andrehugo.bs@gmail.com>
2022-05-15 14:37:26 -0300

    comando previsão do tempo

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from gtts import gTTS
 from io import BytesIO
 from datetime import datetime
-import pygame
 import speech_recognition as sr
+import webbrowser as wb
+import pygame
 import pyttsx3
+import linecache
 
+def abrirNavegador(link):
+  wb.open_new_tab(link)
 
 def falar(texto, language='pt'):
   mp3_fo = BytesIO()
@@ -42,26 +46,42 @@ def cadastrarEventoAgenda(recon):
   except OSError:
     print('Arquivo de agenda não encontrado')
 
-
 def lerAgenda(recon):
   try:
     f = open('agenda.txt', 'r', encoding='utf-8')
+     
     for line in f:
       falar(line)
+      input('Aperte enter para ir ao próximo evento.')
+      print(line)
+    
     f.close()
   except OSError:
     print('Arquivo de agenda não encontrado')
 
 
 def checarHoras(recon):
-  falar('Checando horas...')
+  horas = datetime.today().strftime('%H')
+  minutos = datetime.today().strftime('%M')
+  segundos = datetime.today().strftime('%S')
+  falar('Agora são '+horas+' horas e '+minutos+' minutos e '+segundos+' segundos')
   return 'Checando horas...'
+
+def checarPrevisaoTempo(recon):
+  falar('De qual cidade?')
+  cidade = ouvir(recon)
+  falar('Vou mostrar para você a previsão do tempo da cidade de '+cidade)
+  abrirNavegador('https://www.google.com/search?q=previsão+do+tempo '+cidade)
+  
+def classificacaoBrasileirao(recon):
+  falar('Qual ')
 
 def mapearComandos(comando, recon):
   comandos = {
     ('ler agenda', 'ver agenda', 'quero ver agenda', 'quero ler minha agenda', 'abrir agenda'): lerAgenda,
     ('cadastrar evento na agenda', 'cadastrar novo evento na agenda', 'cadastrar evento'): cadastrarEventoAgenda,
-    ('checar as horas', 'que horas são', 'checar horário'): checarHoras
+    ('checar as horas', 'que horas são', 'checar horário'): checarHoras,
+    ('checar previsão do tempo', 'como vai estar o tempo hoje', 'como está o clima hoje', 'como está o clima'): checarPrevisaoTempo
   }
   for key, value in comandos.items():
     if comando.lower() in key:
@@ -75,14 +95,15 @@ def ouvir(recon):
   return comando
 
 
+engine = pyttsx3.init()
 recon = sr.Recognizer()
 recon.energy_threshold = 50
 recon.dynamic_energy_threshold = False
-engine = pyttsx3.init()
 pygame.mixer.init()
 with sr.Microphone() as source:
   recon.adjust_for_ambient_noise(source, duration=3)
   while True:
+    engine.runAndWait()
     try:
       comando = ouvir(recon)
       if 'sexta-feira' in comando:
@@ -92,7 +113,7 @@ with sr.Microphone() as source:
         comando = ouvir(recon)
         mapearComandos(comando, recon)
         print(comando)
-        esperar = input('Pressione Enter para continuar...')
+        #esperar = input('Pressione Enter para continuar...')
     except sr.UnknownValueError:
       print('\nTente novamente')
       continue
