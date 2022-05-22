8850ae51d8c0eaf45854bed48d25c127c7ce6766
André <andrehugo.bs@gmail.com>
2022-05-22 16:58:20 -0300

    funcionalidade ler historico de compras e consultar cartoes

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from gtts import gTTS
 from io import BytesIO
 from datetime import datetime
+from os.path import exists
 import speech_recognition as sr
 import pygame
-import pyttsx3
-import random
 import json
+import time
 
 recon = sr.Recognizer()
-recon.energy_threshold = 150
-recon.dynamic_energy_threshold = False
 pygame.mixer.init()
 fraseConfirmacao = ' Essa informação está correta?'
 
@@ -83,89 +81,160 @@ def cadastrarCartao():
           'dataValidade': dataValidadeCartao,
           'codigoSeguranca': codigoSegurancaCartao
         }
+        
         print(infoCartao)
+        
+        if exists('carteira.json') == False:
+          file = open('carteira.json', 'w')
+          cartoes = {
+            'cartoes': [
+              infoCartao
+            ]
+          }
+          json.dump(cartoes, file)
+        else:
+          file = open('carteira.json', 'r')
+          data = json.load(file)
+          data["cartoes"].append(infoCartao)
+          file.close()
+          file = open('carteira.json', 'w')
+          json.dump(data, file)
+          file.close()
         falar('O seu cartão foi cadastrado.')
+        time.sleep(3)
         break
       
       except sr.UnknownValueError:
         falar('Desculpe, não entendi o que disse.')
+        time.sleep(3)
         continue
       
   except IOError:
     raise IOError('Arquivo não encontrado.')
 
 def consultarCartoesCadastrados():
-  # try:
-    
-  # except IOError:
-  #   raise IOError('Arquivo não encontrado')
-  return
+  """
+  Consulta todos os cartões cadastrados na carteira.
+
+  Raises:
+      IOError: Aciona essa exception caso o arquivo carteira.json não seja encontrado.
+  """
+  try:
+    if exists('carteira.json') == False:
+      falar('Não há cartões cadastrados.')
+      time.sleep(3)
+    else:
+      file = open('carteira.json', 'r')
+      data = json.load(file)
+      if len(data["cartoes"]) < 1:
+        falar('Não há cartões cadastrados.')
+        time.sleep(3)
+      else:  
+        falar("Lendo os cartões na sua carteira.")
+        time.sleep(3)
+        for cartao in data["cartoes"]:
+          falar("Cartão "+cartao["numero"]+" com data de validade "+cartao["dataValidade"]+" e código de segurança "+cartao["codigoSeguranca"])
+          time.sleep(20)
+          
+  except IOError:
+    raise IOError('Arquivo não encontrado')
 
 def lerHistoricoCompras():
-  return
+  """
+  Lê todo o histórico de compras do usuário.
 
+  Raises:
+      IOError: Aciona essa exception caso o arquivo compras.json não seja encontrado.
+  """
+  try:
+    if exists('compras.json') == False:
+      falar('Nenhuma compra foi realizada.')
+      time.sleep(3)
+    else:
+      file = open('compras.json', 'r')
+      data = json.load(file)
+      if len(data["historico"]) < 1:
+        falar('Nenhuma compra foi realizada.')
+        time.sleep(3)
+      else:  
+        falar("Lendo seu histórico de compras.")
+        time.sleep(3)
+        for compra in data["historico"]:
+          falar("Compra realizada no dia "
+            +compra["dataCompra"]+" às "+compra["horarioCompra"]+" no estabelecimento "
+            +compra["estabelecimento"]+" no valor de "+compra["valorCompra"]
+            +" utilizando o cartão "+compra["cartaoUtilizado"])
+          time.sleep(20)
+          
+  except IOError:
+    raise IOError('Arquivo não encontrado')
 
 def excluirCartao():
-  return
-
-  
-'''
-def cadastrarEventoAgenda(recon):
   try:
-    falar('Ok, qual o nome do evento que devo cadastrar?')
-    nomeEvento = ouvir(recon)
-    engine.runAndWait()
-    falar('Para qual dia?')
-    diaEvento = ouvir(recon)
-    engine.runAndWait()
-    falar('Para que horas?')
-    horarioEvento = ouvir(recon)
-    engine.runAndWait()
-    falar('Tudo certo, o seu evento foi cadastrado!')
-    engine.runAndWait()
-    f = open('agenda.txt', 'a', encoding='utf-8')
-    f.write('Você tem o evento '+nomeEvento+' às '+horarioEvento+' no dia '+diaEvento+'\n')
-    f.close()
-  except OSError:
-    print('Arquivo de agenda não encontrado')
-'''
+    if exists('carteira.json') == False:
+      falar('Nenhum cartão foi encontrado.')
+      time.sleep(3)
+    else:
+      file = open('carteira.json', 'r')
+      data = json.load(file)
+      file.close()
+      if len(data["cartoes"]) < 1:
+        falar('Nenhum cartão foi encontrado.')
+        time.sleep(3)
+      else:
+        falar("Qual o número do cartão que deseja excluir?")
+        numeroCartao = ouvir()
+        cartaoEncontrado = False
+        time.sleep(3)
+        for cartao in data["cartoes"]:
+          if numeroCartao.replace(" ", "") == cartao["numero"].replace(" ", ""):
+            del cartao
+            falar("Cartão "+numeroCartao+" removido!")
+            file = open('carteira.json', 'w')
+            json.dump(data, file)
+            file.close()
+            cartaoEncontrado = True
+            time.sleep(20)
+        
+        if cartaoEncontrado == False:
+          falar("Nenhum cartão com esse número foi encontrado.")
+          
+  except IOError:
+    raise IOError('Arquivo não encontrado')
 
-'''
-def lerAgenda(recon):
-  try:
-    f = open('agenda.txt', 'r', encoding='utf-8')
-     
-    for line in f:
-      falar(line)
-      input('Aperte enter para ir ao próximo evento.')
-      print(line)
-    
-    f.close()
-  except OSError:
-    print('Arquivo de agenda não encontrado')
-'''
-  
 def mapearComandos(comando):
+  """
+  Método utilizado para mapear o comando falado pelo o usuário às funções correspondentes.
+  Caso encontre o comando, executará a função equivalente.
+
+  Args:
+      comando (str): Texto do comando enviado dito pelo o usuário.
+
+  Returns:
+      value (function): Retorna a execução da função correspondente com o comando passado.
+  """
   comandos = {
+    ('abrir histórico de compras', 'ver histórico de compras', 'histórico de compras'): lerHistoricoCompras,
     ('cadastrar novo cartão', 'cadastrar cartão', 'cadastrar cartão de crédito'): cadastrarCartao,
     ('ver cartões cadastrados', 'consultar cartões cadastrados', 'consultar cartões'): consultarCartoesCadastrados,
-    ('abrir histórico de compras', 'ver histórico de compras', 'histórico de compras'): lerHistoricoCompras,
-    ('deletar cartão', 'excluir cartão'): excluirCartao
+    ('deletar cartão', 'excluir cartão', 'remover cartão'): excluirCartao
   }
   
   for key, value in comandos.items():
+    print(key)
+    print(comando.lower())
     if comando.lower() in key:
       return value()
-
+    
+recon.energy_threshold = 180
+recon.dynamic_energy_threshold = False
 with sr.Microphone() as source:
   recon.adjust_for_ambient_noise(source, duration=3)
   while True:
     try:
       falar('O que deseja fazer?')
       comando = ouvir()
-      #esperar = input('Pressione Enter para continuar...')
       mapearComandos(comando)
-      #esperar = input('Pressione Enter para continuar...')
     except sr.UnknownValueError:
       print('\nTente novamente')
       continue
@@ -173,5 +242,7 @@ with sr.Microphone() as source:
       print('\nEncerrando...')
       break
     except KeyError:
+      falar('Comando não encontrado.')
+      time.sleep(3)
       print('\n Comando não encontrado')
       continue
--- teste.py
+++ teste.py
@@ -8,20 +8,15 @@ import pyttsx3
 import random
 import json
 
-engine = pyttsx3.init()
-recon = sr.Recognizer()
-recon.energy_threshold = 50
-recon.dynamic_energy_threshold = False
-pygame.mixer.init()
+# file = open('carteira.json', 'r')
+# data = json.load(file)
+# data["cartoes"].append({'numero': '4716 6151 2634 4615', 'dataValidade': '22/02/2023', 'codigoSeguranca': '117'})
+# print(data)
+# file.close()
+# file = open('carteira.json', 'w')
+# json.dump(data, file)
+# file.close()
 
-def falar(texto, language='pt'):
-  mp3_fo = BytesIO()
-  tts = gTTS(texto, lang=language)
-  tts.write_to_fp(mp3_fo)
-  pygame.mixer.music.load(mp3_fo, 'mp3')
-  pygame.mixer.music.play()
-  #engine.runAndWait()
+teste = 'histórico de compras' in ('abrir histórico de compras', 'ver histórico de compras', 'histórico de compras')
 
-file = open("agenda.txt", "r").read().replace("\n", " ")
-falar(str(file))
-entrada = input()
+print(teste)
